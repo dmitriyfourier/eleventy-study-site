f11ba1d10e5f84f006e31fb137fc88337d1e131e
Dmitriy Fourier <dmitriyfurye@gmail.com>
2022-07-21 15:03:37 +0500

    Fisrt Commit

---
+++ src/_data/site.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"title": "Eleventy Web Starter",
+	"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PostCSS and esbuild",
+	"url": "https://furye.ru",
+	"author": "Dmitriy Fourier",
+	"author_Ru": "Дима Фурье",
+	"blog_URL": "https://blog.furye.ru",
+	"meta_data": {
+		"twitter": "@dmitriyfourier",
+		"opengraph_default": "/static/opengraph-default.jpg"
+	},
+	"env": process.env.ELEVENTY_ENV === 'production'
+}
