5791ed032cadfc72abc77785fb77fe6c15297e3c
Dmitriy Fourier <dmitriyfurye@gmail.com>
2022-07-22 17:13:59 +0500

    Add podacasts pages
    experiments with collections

---
+++ src/_data/social.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"title": "Eleventy Web Starter",
+    "telegram": "https://t.me/FourierNotesRU",
+    "inst": "https://www.instagram.com/dmitriyfourier",
+    "vk":"https://vk.com/dmitriyfourier",
+    "fb":"https://www.facebook.com/DmitriyFourier",
+    "yt":"",
+    "twi":"https://twitter.com/dmitriyfourier",
+    "linked":"https://www.linkedin.com/in/dmitriyfourier/",
+    "unt":"https://untappd.com/user/DmitriyFourier",
+    "myshows":"https://myshows.me/DmitriyFourier",
+    "yandex":"https://reviews.yandex.ru/user/dmitriyfourier",
+	"env": process.env.ELEVENTY_ENV === 'production'
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 module.exports = {
 	theme: {
 		fontFamily: {
-			'sans': ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+			'sans': ['Open Sans', 'Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
 		},
 		extend: {
 			colors: {
-				'ws-blue': {
-					100: '#E6F0FF',
-					200: '#BFDAFF',
-					300: '#99C3FF',
-					400: '#4D97FE',
-					500: '#006AFE',
-					600: '#005FE5',
-					700: '#004098',
-					800: '#003072',
-					900: '#00204C',
+				'ws-link': {
+					main:'#0877db',
+					main_u: 'rgba(43, 154, 255, 0.25)',
+					hover: '#2b9aff',
+					hover_u: 'rgba(43, 154, 255, 0.25)',
+					focus: '#2b9aff',
+					focus_u: 'rgba(43, 154, 255, 0.25);',
+					active: '#bc4df8',
+					active_u: 'rgba(188, 77, 248, 0.25)',
+					visited:'#9108db',
+					visited_u:'rgba(145, 8, 219, 0.25)',
+
 				},
 			}
 		},
